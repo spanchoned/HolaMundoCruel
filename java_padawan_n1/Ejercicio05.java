efa8e8c73869c450dc203e41c75c45d0440e80b1
spanchoned <130292239+spanchoned@users.noreply.github.com>
2023-09-25 17:17:42 -0300

    newCase

--- java_padawan_n1/Ejercicio05.java
+++ java_padawan_n1/Ejercicio05.java
@@ -12,9 +12,9 @@ public class Ejercicio05 {
         System.out.println("y una última ");
         double libra3= scanner.nextDouble();
 
-        final double kilogramo=0.45359237;
+        final double KILOGRAMO=0.45359237;
       
 
-        System.out.println("la conversion de sus libras es " + (kilogramo*libra1) + " kg,  " + (kilogramo*libra2) + " kg, " + (kilogramo*libra3) + " kg");
+        System.out.println("la conversion de sus libras es " + (KILOGRAMO*libra1) + " kg,  " + (KILOGRAMO*libra2) + " kg, " + (KILOGRAMO*libra3) + " kg");
     }
 }

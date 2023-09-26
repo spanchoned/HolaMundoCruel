f31f5c439a28d0e30b69bbcfe9fcfc56287d221e
spanchoned <130292239+spanchoned@users.noreply.github.com>
2023-09-26 18:17:19 -0300

    26/09-maestro-N1-s/com

--- javaMaestroN1/EjercicioThree.java
+++ javaMaestroN1/EjercicioThree.java
@@ -23,20 +23,3 @@ public class EjercicioThree {
 
 
 
-
-
-
-
-/*La hipotenusa de un triángulo rectángulo se calcula utilizando el teorema de Pitágoras. En un triángulo rectángulo,
-tienes dos catetos (los lados que forman el ángulo recto) y una hipotenusa (el lado opuesto al ángulo recto).
-El teorema de Pitágoras establece lo siguiente:
-En un triángulo rectángulo, la suma de los cuadrados de los dos catetos es igual al cuadrado de la hipotenusa.
-Matemáticamente, esto se expresa de la siguiente manera:
-
-a^2 + b^2 = c^2
-
-Donde:
-"a" y "b" son las longitudes de los catetos.
-"c" es la longitud de la hipotenusa.
-Para calcular la hipotenusa, simplemente toma la raíz cuadrada de la suma de los cuadrados de los catetos:
-c = √(a^2 + b^2)*/
--- javaMaestroN1/EjercicioTwo.java
+++ javaMaestroN1/EjercicioTwo.java
@@ -2,7 +2,7 @@ package javaMaestroN1;
 
 public class EjercicioTwo {
     public static void main(String[] args) {
-        double celsius = 1.0; // Cambia este valor según la temperatura que quieras convertir
+        double celsius = 1.0;
         double fahrenheit = (celsius * 9/5) + 32;
 
         System.out.println("La conversión de " + celsius + " grados Celsius a grados Fahrenheit es: " + fahrenheit + " grados");
